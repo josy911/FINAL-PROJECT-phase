ad54943b7ce905250a0ba4108df9017853238e4d
peters <ptrsbnn@gmail.com>
2024-01-17 17:24:43 +0100

    main.js commented

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,11 +8,11 @@ import store from "./store";
 // import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 
-const options = {
-  timeout: 5000,
-  position: positions.BOTTOM_CENTER,
-  transition: transitions.SCALE,
-};
+// const options = {
+//   timeout: 5000,
+//   position: positions.BOTTOM_CENTER,
+//   transition: transitions.SCALE,
+// };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
