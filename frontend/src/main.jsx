77779ec3284c5a11b122a80b47759c5b882593e0
peters <ptrsbnn@gmail.com>
2024-01-17 17:23:28 +0100

    react-alert removed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
-import { positions, transitions, Provider as AlertProvider } from "react-alert";
+// import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 
 const options = {
@@ -16,8 +16,8 @@ const options = {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <AlertProvider template={AlertTemplate} {...options}>
-      <App />
-    </AlertProvider>
+    {/* <AlertProvider template={AlertTemplate} {...options}> */}
+    <App />
+    {/* </AlertProvider> */}
   </Provider>
 );
